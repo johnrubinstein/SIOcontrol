faa474fc1c0cf24ac12c6e96f6c73b870dfdf661
johnrubinstein <john.rubinstein@utoronto.ca>
2017-12-28 16:34:19 +0000

    tested on SIO

--- SIOcontrol.py
+++ SIOcontrol.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python
 
-
 # Uncomment for use of pi
 import RPi.GPIO as GPIO
 import Adafruit_DHT
@@ -12,13 +11,6 @@ def applysample(pin_cannon,wait,duration):
     GPIO.output(pin_cannon,GPIO.HIGH)
     time.sleep(duration)
     GPIO.output(pin_cannon,GPIO.LOW)
-
-def cleancannon(pin_cannon):
-    for x in range(5):
-        GPIO.output(pin_cannon,GPIO.HIGH)
-        time.sleep(0.1)
-        GPIO.output(pin_cannon,GPIO.LOW)
-        time.sleep(0.5)
     
 def releaseplunger(pin_plunger,wait):
     time.sleep(wait)
@@ -37,7 +29,6 @@ if __name__=='__main__':
     parser.add_argument('--sdelay',    help='Time to wait before applying (seconds)',default = 0, type=float,required=False)
     parser.add_argument('--pdelay',    help='Time to wait before plunging (seconds)',default = 0, type=float,required=False)
     args = parser.parse_args()
-
     
     # Define pins
     pin_cannon  = 12
@@ -52,20 +43,19 @@ if __name__=='__main__':
 
     # Report environmental conditions
     humidity, temperature = readenvironment(pin_dht22)
-    print('{0:0.1f} Temp={1:0.1f}\'C  Humidity={2:0.1f}% RH'.format(tic-starttime,temperature, humidity))
+    print('Temp={0:0.1f}\'C  Humidity={1:0.1f}% RH'.format(temperature, humidity))
 
     # Breakpoint
     raw_input("Press Enter to continue...")
     
     # set up processes
-    sample = threading.Thread(target=applysample, args=(pin_cannon,args.sdelay,args.stime))  # defines the thread
-    plunger = threading.Thread(target=releaseplunger, args=(pin_plunger,args.pdelay))  # defines the thread
+    sample = threading.Thread(target=applysample, args=(pin_cannon,args.sdelay,args.stime))  
+    plunger = threading.Thread(target=releaseplunger, args=(pin_plunger,args.pdelay))  
 
     # start processes
     plunger.start()  
     sample.start()  
 
-    
     # Exit with reset of plunger
     raw_input("Press Enter to continue...")
     resetplunger(pin_plunger)
