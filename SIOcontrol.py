c61e39253164c97d854848011561e9616d8053ea
John Rubinstein <john.rubinstein@utoronto.ca>
2018-10-17 14:12:49 -0400

    add the cannon forward and reverse solenoid

--- SIOcontrol.py
+++ SIOcontrol.py
@@ -7,6 +7,13 @@ import time, threading
 import argparse
 import sys, select
 
+def cannonforward(pin_cannonposition):
+    GPIO.output(pin_cannonposition,GPIO.HIGH)
+
+def cannonreverse(pin_cannonposition,cannonretreatdelay):
+    time.sleep(cannonretreatdelay)
+    GPIO.output(pin_cannonposition,GPIO.HIGH)
+
 def applysample(pin_cannon,wait,duration):
     time.sleep(wait)
     GPIO.output(pin_cannon,GPIO.HIGH)
@@ -18,7 +25,7 @@ def releaseplunger(pin_plunger,wait):
     GPIO.output(pin_plunger,GPIO.HIGH)
 
 def resetplunger(pin_plunger):
-     GPIO.output(pin_plunger,GPIO.LOW)
+    GPIO.output(pin_plunger,GPIO.LOW)
     
 def readenvironment(pin_dht22):
     humidity, temperature = Adafruit_DHT.read_retry(22, pin=pin_dht22)
@@ -33,11 +40,15 @@ if __name__=='__main__':
     args = parser.parse_args()
     
     # Define pins
-    pin_cannon  = 12
-    pin_plunger = 19
-    pin_dht22   = 24
-    timeout = 1     # withdraw the plunger to avoid overheating
-    kuhnketime = 1
+    pin_cannon         = 12
+    pin_plunger        = 19
+    pin_dht22          = 24
+    pin_cannonposition = 21
+
+    # Default timing
+    cannonretreatdelay = 0.1+args.pdelay
+    timeout            = 1     # withdraw the plunger to avoid overheating
+    kuhnketime         = 1
     
     GPIO.setwarnings(False)
     GPIO.cleanup()    
@@ -49,37 +60,26 @@ if __name__=='__main__':
     humidity, temperature = readenvironment(pin_dht22)
     print('Temp={0:0.1f}\'C  Humidity={1:0.1f}% RH'.format(temperature, humidity))
 
+    # put cannon into place
+    cannonforward(pin_cannonposition)
+    
     # Breakpoint
     raw_input("Press Enter to continue...")
 
-
-    
-    
     # set up processes
     sample = threading.Thread(target=applysample, args=(pin_cannon,args.sdelay,args.stime))  
     plunger = threading.Thread(target=releaseplunger, args=(pin_plunger,args.pdelay))  
-
+    plunger = threading.Thread(target=cannonreverse, args=(pin_cannonreverse,cannonreversedelay)) 
+    
     # start processes
     if not args.donotplunge:
         plunger.start()
         
     sample.start()  
 
-    
-
     # Kuhnke plunger
     time.sleep(kuhnketime+args.pdelay+args.sdelay)
     resetplunger(pin_plunger)
-    
-    # Exit with reset of plunger (not needed for Kuhnke plunger)
-    #print "Press Enter to continue [will time out in {0} seconds]...".format(timeout)
-    #i, o, e = select.select( [sys.stdin], [], [], timeout )
-    #if (i):
-    #  print "Done!", sys.stdin.readline().strip()
-    #else:
-    #  print "Time out!"
-    #resetplunger(pin_plunger)
-
     
     print "Done!"
     
