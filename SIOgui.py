e0c258f45694825478428a88b467ace1f0ccc801
John Rubinstein <john.rubinstein@utoronto.ca>
2019-03-28 10:45:51 -0400

    add GUI control of power

--- SIOgui.py
+++ SIOgui.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 from guizero import App, TextBox, Text, PushButton, CheckBox
-from subprocess import call
+from subprocess import call, Popen
 
 def startprocess():
     print("starting process")
@@ -13,13 +13,24 @@ def startprocess():
     #print(arguments)
     call(arguments)
 
+def powerup():
+    print("Power up")
+    arguments = ["python3","SIOpowerupdown.py","--updown","up"]
+    call(arguments)
+    
+def powerdown():
+    print("Power down")
+    arguments = ["python3","SIOpowerupdown.py","--updown","down"]
+    call(arguments)
+    
 def cleanprocess():
     print("starting clean process")
     spraytime  = str(float(cleantime.value)/1000)
     cycles = cleancycles.value
     arguments = ["python3","cleancontrol.py","--stime",spraytime,"--cycles",cycles]
     #print(arguments)
-    call(arguments)
+    #call(arguments)
+    Popen(arguments)
     #call(["python3","cleancontrol.py","--stime",stime,"--cycles",cycles])
 
 app = App(title="Shake-it-off", layout="grid")
@@ -29,7 +40,9 @@ stime       = TextBox(app, grid=[1,1], text="5")
 pdelaylabel = Text(app, text="Plunge delay (ms)", grid=[0,2])
 pdelay      = TextBox(app, grid=[1,2], text="5")
 donotplunge = CheckBox(app, text="Do not plunge", grid=[0,3])
-start       = PushButton(app, command=startprocess, text="Start Process", grid=[0,4])
+button_up   = PushButton(app, command=powerup,text="On", grid=[0,4])
+button_down = PushButton(app, command=powerdown, text="Off", grid=[1,4])
+button_start= PushButton(app, command=startprocess, text="Start Process", grid=[0,5])
 
 cleancycleslabel = Text(app, text="Cleaning cycles", grid=[3,1])
 cleancycles      = TextBox(app, text="5",grid=[4,1])   
