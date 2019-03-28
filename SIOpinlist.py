3b6238c79717978574a005befa532e61c39ddd8f
John Rubinstein <john.rubinstein@utoronto.ca>
2019-03-28 10:13:00 -0400

    oops, added the missing pin list

---
+++ SIOpinlist.py
@@ -0,0 +1,8 @@
+# this file contains a list of RPi GPIO pins used by SIO
+cannon         = 25 #
+plunger        = 19 #
+dht22          = 24 # Temperature and humidity monitor - currently not used
+cannonposition = 26 #
+sensorpower    = 12 #
+irsensor       = 16 #
+interlock      = 27 #
